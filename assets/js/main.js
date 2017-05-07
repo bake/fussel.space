4a0ef4ee49afb886fdc8112fecde14d3b3aa9f4f
bake <bake@192k.pw>
2017-05-07 17:22:43 +0200

    meta-foo under articles

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,28 @@
 'use strict'
 
-let highlight_current = (path) => new Promise((resolve, reject) => {
-	let nav = document.querySelector(`nav a[href="${path}"]`)
+const highlight_current = path =>
+	new Promise((resolve, reject) => {
+		const nav = document.querySelector(`nav a[href="${path}"]`)
 
-	if(nav) {
-		nav.classList.add('active')
-		localStorage.setItem(path, true)
-	}
-
-	resolve()
-})
-
-let highlight_read = () => new Promise((resolve, reject) => {
-	for(let node of [...document.querySelectorAll('nav a[href^="/"]')]) {
-		let path = node.getAttribute('href')
-
-		if(!localStorage.getItem(path)) {
-			node.classList.add('new')
+		if (nav) {
+			nav.classList.add('active')
+			localStorage.setItem(path, true)
 		}
-	}
 
-	resolve()
-})
+		resolve()
+	})
+
+const highlight_read = () =>
+	new Promise((resolve, reject) => {
+		for (const node of [...document.querySelectorAll('nav a[href^="/"]')]) {
+			const path = node.getAttribute('href')
+
+			if (!localStorage.getItem(path)) {
+				node.classList.add('new')
+			}
+		}
+
+		resolve()
+	})
 
 highlight_current(window.location.pathname).then(highlight_read)
